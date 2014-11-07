61cdd5249d6f7d0c4a87b4140365c0efac99bcb7
AGodinez <pistilloagodinez@gmail.com>
2014-11-06 19:01:47 -0600

    Agregue countdown para CPSLV1

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,19 @@ jQuery(function($) {
 			 scrollTop: $("body").offset().top
 		 }, 500);
 	});	
-	//End goto top		
+	//End goto top	
+
+	
 
 });
+
+//Contador Campus Party El Salvador
+function getTime() {
+	hoy = new Date();
+	cpslv = new Date("Nov 27 2014 15:00:00");
+	dias = (cpslv - hoy) / 1000 / 60 / 60 / 24;
+	contarDia = Math.floor(dias);
+	dy = (contarDia == 1) ? " d&iacute;a, " : " d&iacute;as, "
+	document.getElementById("contador").innerHTML = "Estamos a " + contarDia + dy + "participa en la rifa de entradas <a href='http://goo.gl/MjdnCq'>AQUÍ</a>";
+	newTiempo = window.setTimeout("getTime();", 1000);
+}
